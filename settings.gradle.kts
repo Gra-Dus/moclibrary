3dee1bad1966314b47a68f69205813177e70a405
Gra_Dus <65005478+Gra-Dus@users.noreply.github.com>
2023-08-30 10:12:39 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,17 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id.startsWith("com.android")) {
+                useModule("com.android.tools.build:gradle:8.0.0")
+            }
+
+            if (requested.id.id.startsWith("org.jetbrains.kotlin")) {
+                useVersion("1.8.10")
+            }
+        }
+    }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
